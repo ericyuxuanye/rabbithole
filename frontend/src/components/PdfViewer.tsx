71d7299bb60d6f0bade6f9485853daa77c510808
Ray Chen <raychen256@gmail.com>
2024-10-12 22:38:34 -0700

    add highlight plugin

--- frontend/src/components/PdfViewer.tsx
+++ frontend/src/components/PdfViewer.tsx
@@ -1,24 +1,56 @@
 import { Viewer, Worker } from "@react-pdf-viewer/core";
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/highlight/lib/styles/index.css";
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
+import {
+  highlightPlugin,
+  MessageIcon,
+  RenderHighlightTargetProps,
+} from "@react-pdf-viewer/highlight";
+import { Button, Position, Tooltip } from "@react-pdf-viewer/core";
 
 interface PdfViewerProps {
   pdfUrl: string;
 }
-
 export default function PdfViewer({ pdfUrl }: PdfViewerProps) {
+  const renderHighlightTarget = (props: RenderHighlightTargetProps) => (
+    <div
+      style={{
+        background: "#eee",
+        display: "flex",
+        position: "absolute",
+        left: `${props.selectionRegion.left}%`,
+        top: `${props.selectionRegion.top + props.selectionRegion.height}%`,
+        transform: "translate(0, 8px)",
+      }}
+    >
+      <Tooltip
+        position={Position.TopCenter}
+        target={
+          <Button onClick={props.toggle}>
+            <MessageIcon />
+          </Button>
+        }
+        content={() => <div style={{ width: "100px" }}>Add a note</div>}
+        offset={{ left: 0, top: -8 }}
+      />
+    </div>
+  );
+
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const highlightPluginInstance = highlightPlugin({
+    renderHighlightTarget,
+  });
   return (
-    <div style={{height: "100%"}}>
+    <div style={{ height: "100%" }}>
       <Worker
         workerUrl={`https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js`}
       >
         <Viewer
           fileUrl={pdfUrl}
-          plugins={[defaultLayoutPluginInstance]}
+          plugins={[defaultLayoutPluginInstance, highlightPluginInstance]}
         />
       </Worker>
     </div>
